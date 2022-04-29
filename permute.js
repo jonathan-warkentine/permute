4d123895cbbeaac8ae70aba74584d87a40863c83
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-04-28 23:16:23 -0400

    merged the two permutating functions, created a split function

--- permute.js
+++ permute.js
@@ -1,46 +1,33 @@
-function permute (ar) { 
-//accepts an array of length 2 or 3 and returns an array of arrays containing all permutations
-//if the array is longer the function splits the argument and recursively calls itself for each
+function split (ar) {
+  if (ar.length != 1){
+    //split the array and permute each half
+    const ar1 = ar.slice(0, Math.floor(ar.length/2));
+    const ar2 = ar.slice(Math.floor(ar.length/2), ar.length);
 
-if (ar.length > 3){
-  //split the array and permute each half
-  const ar1 = ar.slice(0, Math.floor(ar.length/2));
-  const ar2 = ar.slice(Math.floor(ar.length/2), ar.length);
-
-  return megaPermute(permute(ar1), permute(ar2));
-}
-
-//permute
-let permutations = [];
-permutations.push(ar.flat()); //covers both [ar[0], ar[1]] or [ar[0], ar[1], ar[2]]
-// console.log(1, permutations)
-ar.forEach(el => { //pushes each individual array element into the array
-  permutations.push([el]);
-});
-// console.log(2, permutations)
-if (ar[2]) {
-  permutations.push([ar[1], ar[2]]);
-  permutations.push([ar[0], ar[1]]);
-  permutations.push([ar[0], ar[2]]);
-}
-return permutations;
-}
-
-function megaPermute(ar1, ar2){ //function will be receiving an array of arrays
-let possibilities = [];
-// console.log('ar1: ', ar1, '\nar2: ', ar2)
-mergeArray(ar1, ar2).forEach(poss => {
-  possibilities.push(poss);
-})
-for (let i=0; i<ar1.length; i++){
-  for (let j=0; j<ar2.length; j++){
-    possibilities.push(mergeArray(ar1[i], ar2[j]));
+    return permute(split(ar1), split(ar2));
   }
+
+  return [ar];
 }
 
-return possibilities;
+function permute (ar1, ar2) { 
+
+  let possibilities = [];
+  
+  mergeArray(ar1, ar2).forEach(poss => {
+    possibilities.push(poss);
+  });
+
+  for (let i=0; i<ar1.length; i++){
+    for (let j=0; j<ar2.length; j++){
+      possibilities.push(mergeArray(ar1[i], ar2[j]));
+    }
+  }
+
+  return possibilities;
 }
+
 
 function mergeArray (ar1, ar2) {
-return [ar1, ar2].flat();
+  return [ar1, ar2].flat();
 }
